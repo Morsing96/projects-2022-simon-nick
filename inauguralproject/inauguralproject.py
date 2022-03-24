465d755b4ef023fe7e4a6b6dc0910c70eafe7d77
NickDahl91 <nd120391@gmail.com>
2022-03-24 14:44:46 +0100

    Tilføjet de første funktioner

--- inauguralproject/inauguralproject.py
+++ inauguralproject/inauguralproject.py
@@ -1,15 +1,9 @@
-def square(x):
-    """ square numpy array
-    
-    Args:
-    
-        x (ndarray): input array
-        
-    Returns:
-    
-        y (ndarray): output array
-    
-    """
-    
-    y = x**2
-    return y
+def ins_exp_util(p, q, y, x) :
+    return p * util(y - x + q - pi(p,q), theta) + (1 - p) * util(y - pi(p,q), theta)
+
+def pi(p, q) :
+    return p * q
+
+def util(z, theta):
+    return (z ** (1 + theta)) / (1 + theta)
+
